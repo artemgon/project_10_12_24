db719a4a48d604be20f4d87d9f2941373de06f3c
artemgon <artemhoncharuk123@gmail.com>
2025-01-12 14:51:27 +0200

    Task 3 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,118 @@
 ﻿
+using System.Diagnostics;
+
 namespace project_10_12_24.Classes
 {
-    internal class Classes
+    internal class City
     {
+        private string CityName { get; set; }
+        public string CountryName { get; set; }
+        public string PhoneCode { get; set; }
+        public List<string> CityDistricts { get; set; }
+        public int Population { get; set; }
+        public string City_Name
+        {
+            private get => CityName;
+            set => CityName = value;
+        }
+        public City()
+        {
+            CityName = "Unknown";
+            CountryName = "Unknown";
+            PhoneCode = "Unknown";
+            CityDistricts = [];
+            Population = 0;
+        }
+        public City(string name) : this()
+        {
+            CityName = name;
+        }
+        public City(string name, string country) : this(name)
+        {
+            CountryName = country;
+        }
+        public City(string name, string country, string code) : this(name, country)
+        {
+            PhoneCode = code;
+        }
+        public City(string name, string country, string code, List<string> districts) : this(name, country, code)
+        {
+            CityDistricts = districts;
+        }
+        public City(string name, string country, string code, List<string> districts, int population) : this(name, country, code, districts)
+        {
+            Population = population;
+        }
+        public string? this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < CityDistricts.Count)
+                {
+                    return CityDistricts[index];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < CityDistricts.Count && value != null)
+                {
+                    CityDistricts[index] = value;
+                }
+            }
+        }
+        public static City operator + (City city, int pop)
+        {
+            city.Population += pop;
+            return city;
+        }
+        public static City operator -(City city, int pop)
+        {
+            city.Population -= pop;
+            return city;
+        }
+        public static bool operator ==(City city1, City city2)
+        {
+            return city1.CityName == city2.CityName;
+        }
+        public static bool operator <(City city1, City city2)
+        {
+            return city1.Population < city2.Population;
+        }
+        public static bool operator >(City city1, City city2)
+        {
+            return city1.Population > city2.Population;
+        }
+        public static bool operator !=(City city1, City city2)
+        {
+            return city1.CityName != city2.CityName;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is City city)
+            {
+                return this.Population == city.Population;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.Population.GetHashCode();
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("City Name: " + CityName);
+            Console.WriteLine("Country Name: " + CountryName);
+            Console.WriteLine("Phone Code: " + PhoneCode);
+            Console.WriteLine("City Districts: ");
+            foreach (string district in CityDistricts)
+            {
+                Console.WriteLine(district);
+            }
+            Console.WriteLine("Population: " + Population);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,17 @@ namespace project_10_12_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                List<string> districts = [ "Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island" ];
+                City city = new ("New York", "USA", "+1", districts , 8_336_817);
+                //Console.WriteLine($"City: {city.City_Name}"); - city name is private, so inaccesible
+                Console.WriteLine($"Country: {city.CountryName}");
+                Console.WriteLine($"Phone code: {city.PhoneCode}");
+                Console.WriteLine($"Population: {city.Population}");
+                Console.WriteLine("Districts:");
+                for (int i = 0; i < city.CityDistricts.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {city[i]}");
+                }
             }
             catch (Exception ex)
             {
