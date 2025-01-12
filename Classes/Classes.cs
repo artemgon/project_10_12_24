148b167d3d9b83a83c1d675754f1fb417bbf056c
artemgon <artemhoncharuk123@gmail.com>
2025-01-12 12:12:02 +0200

    Task 1 is done.

--- Classes/Classes.cs
+++ Classes/Classes.cs
@@ -1,7 +1,111 @@
 ﻿
 namespace project_10_12_24.Classes
 {
-    internal class Classes
+    public class Employee
     {
+        public string SNP { get; set; }
+        public string DateOfBirth { get; set; }
+        public string PhoneNumber { private get; set; }
+        public string WorkEmail { private get; set; }
+        public string Position { private get; set; }
+        public string Duties { private get; set; }
+        public int Salary { private get; set; } 
+        public Employee()
+        {
+            SNP = "Unknown";
+            DateOfBirth = "Unknown";
+            PhoneNumber = "Unknown";
+            WorkEmail = "Unknown";
+            Position = "Unknown";
+            Duties = "Unknown";
+            Salary = 0;
+        }
+        public Employee(string snp) : this()
+        {
+            SNP = snp;
+        }
+        public Employee(string snp, string dateOfBirth) : this(snp)
+        {
+            DateOfBirth = dateOfBirth;
+        }
+        public Employee(string snp, string dateOfBirth, string phoneNumber) : this(snp, dateOfBirth)
+        {
+            PhoneNumber = phoneNumber;
+        }
+        public Employee(string snp, string dateOfBirth, string phoneNumber, string workEmail) : this(snp, dateOfBirth, phoneNumber)
+        {
+            WorkEmail = workEmail;
+        }
+        public Employee(string snp, string dateOfBirth, string phoneNumber, string workEmail, string position) : this(snp, dateOfBirth, phoneNumber, workEmail)
+        {
+            Position = position;
+        }
+        public Employee(string snp, string dateOfBirth, string phoneNumber, string workEmail, string position, string duties) : this(snp, dateOfBirth, phoneNumber, workEmail, position)
+        {
+            Duties = duties;
+        }
+        public Employee(string snp, string dateOfBirth, string phoneNumber, string workEmail, string position, string duties, int salary) : this(snp, dateOfBirth, phoneNumber, workEmail, position, duties)
+        {
+            Salary = salary;
+        }
+        public static Employee operator +(Employee employee, int sum)
+        {
+            employee.Salary += sum;
+            return employee;
+        }
+        public static Employee operator -(Employee employee, int sum)
+        {
+            employee.Salary -= sum;
+            return employee;
+        }
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.SNP == employee2.SNP;
+        }
+        public static bool operator <(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary < employee2.Salary;
+        }
+        public static bool operator >(Employee employee1, Employee employee2)
+        {
+            return employee1.Salary > employee2.Salary;
+        }
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.SNP != employee2.SNP;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Employee employee)
+            {
+                return this.Salary == employee.Salary;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Salary.GetHashCode();
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Employee info:");
+            Console.WriteLine($"SNP: {SNP}");
+            Console.WriteLine($"Date of birth: {DateOfBirth}");
+            Console.WriteLine($"Phone number: {PhoneNumber}");
+            Console.WriteLine($"Work email: {WorkEmail}");
+            Console.WriteLine($"Position: {Position}");
+            Console.WriteLine($"Duties: {Duties}");
+            Console.WriteLine($"Salary: {Salary}");
+            Console.WriteLine();
+        }
+        public void UpdateInfo(string phoneNumber, string workEmail, string position, string duties, int salary)
+        {
+            PhoneNumber = phoneNumber;
+            WorkEmail = workEmail;
+            Position = position;
+            Duties = duties;
+            Salary = salary;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,17 @@ namespace project_10_12_24
             Console.ForegroundColor = ConsoleColor.DarkRed;
             try
             {
+                Employee employee = new("John Doe", "01.01.2000", "+1234567890", "@work.email", "Manager", "Manage", 1000);
+                Employee employee1 = new("John Doe", "01.01.2000", "+1234567890", "@work.email", "Manager", "Manage", 2000);
+                employee.ShowInfo();
+                employee1.ShowInfo();
+                int sum = 1000;
+                employee += sum;
+                employee1 -= sum;
+                Console.WriteLine(employee == employee1);
+                Console.WriteLine(employee != employee1);
+                Console.WriteLine(employee > employee1);
+                Console.WriteLine(employee < employee1);
             }
             catch (Exception ex)
             {
